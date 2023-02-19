77cd99396485524cc7a20b03a531c91969b86ab0
Ali Abbasov <ali3abbasov@gmail.com>
2023-02-19 13:06:52 +0400

    Initial

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title> Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptates mollitia cumque doloremque a suscipit debitis magnam obcaecati provident eos.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam voluptates mollitia cumque doloremque a suscipit debitis magnam obcaecati provident eos.</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
